7f4e7004a019691f7a13ebb4a716ad369a2a800a
wallace <wallace.wong@bayes.city.ac.uk>
2024-11-10 17:12:17 +0000

    returns.py updated wrangling format

--- scripts/returns.py
+++ scripts/returns.py
@@ -8,14 +8,15 @@ from scipy import stats
 from sklearn import linear_model
 from matplotlib import pyplot as plt
 
-returns_df = pd.read_excel(config.returns, index_col=0)
+returns_df = pd.read_excel(config.returns, skiprows=4, index_col=0)
+returns_df.drop(returns_df.index[1:2], axis=0, inplace=True)
+returns_df.drop(returns_df.index[14:], axis=0, inplace=True)
+returns_df.drop(returns_df.index[1], axis=0, inplace=True)
 
 # returns on equity
 roe_df = returns_df.loc[['ROE Comm Eqty, %, TTM']]
 roe_df = roe_df.T
-# roe_df['ROE Comm Eqty, %, TTM'] = roe_df['ROE Comm Eqty, %, TTM'].apply(lambda x: x*100)
 
 # returns on assets
 roa_df = returns_df.loc[['ROA Tot Assets, %, TTM']]
 roa_df = roa_df.T
-# roe_df['ROA Tot Assets, %, TTM'] = roe_df['ROA Tot Assets, %, TTM'].apply(lambda x: x*100)
